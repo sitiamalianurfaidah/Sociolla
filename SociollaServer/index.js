8d31fb8b7f433574b64d2b3abb26279c64adbdbb
sitiamalianurfaidah <150977479+sitiamalianurfaidah@users.noreply.github.com>
2025-05-02 02:38:36 +0700

    Environment Backend

--- SociollaServer/index.js
+++ SociollaServer/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 const upload = require('./src/utils/upload.middleware'); 
 
 const corsOptions = {
-    origin: 'http://localhost:5050',  
+    origin: 'https://sociolla.vercel.app',  
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],  
     optionsSuccessStatus: 200, 
