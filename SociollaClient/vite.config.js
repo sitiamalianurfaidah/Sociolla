2e957695a75a20ef283f8e65c79353511cd3aa72
sitiamalianurfaidah <150977479+sitiamalianurfaidah@users.noreply.github.com>
2025-05-01 15:57:35 +0700

    Step 1 - Setup Tools

--- SociollaClient/vite.config.js
+++ SociollaClient/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5050
-  }
+    port: 5050,
+    proxy: {
+      '/store': 'http://localhost:5000',
+      '/user': 'http://localhost:5000',
+      '/item': 'http://localhost:5000',
+      '/transaction': 'http://localhost:5000',
+    },
+  },
 })
